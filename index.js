b979cee74440562fa450bde15c1e359f1a8a042c
pi <server@benj4min.com>
2018-01-11 01:17:37 +0000

    server

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var name = 'benjamin';
-console.log('hello: ' + name);
+var http = require('http');
+
+var server = http.createServer( function (req,res ) {
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("Hello World");
+});
+
+server.listen(3000);
+
+console.log("server running at 192.168.0.37:3000");
